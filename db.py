2fb00e39e08f12d18c0cb367bc2fa690f9135ff0
Teja Yerroju <24f3002471@ds.study.iitm.ac.in>
2026-06-02 13:52:56 +0530

    Add DB_PORT support for cloud MySQL providers

--- db.py
+++ db.py
@@ -8,6 +8,7 @@ load_dotenv()
 def get_db_connection():
     return mysql.connector.connect(
         host=os.getenv("DB_HOST", "localhost"),
+        port=int(os.getenv("DB_PORT", "3306")),
         user=os.getenv("DB_USER", "inventory_user"),
         password=os.getenv("DB_PASSWORD", ""),
         database=os.getenv("DB_NAME", "inventory_db")
